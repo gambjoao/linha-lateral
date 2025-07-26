282ff625e118337a2972e5e70a39f8c306c6c6fb
LDB <gambjoao@gmail.com>
2025-07-26 14:30:39 +0100

    Removed unused fonts and metadata import on layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,12 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Inter } from "next/font/google";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
 const inter = Inter({
   variable: "--font-inter",
   subsets: ["latin"],
   display: "swap",
 })
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "Linha Lateral",
